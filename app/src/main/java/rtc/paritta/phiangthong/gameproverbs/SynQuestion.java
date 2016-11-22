e902cc6bcad9c50cb625b64bc7016cacc971ee35
masterUNG <phrombutr@gmail.com>
2016-11-22 14:57:17 +0700

    Setup Permission finish

--- app/src/main/java/rtc/paritta/phiangthong/gameproverbs/SynQuestion.java
+++ app/src/main/java/rtc/paritta/phiangthong/gameproverbs/SynQuestion.java
@@ -1,8 +1,33 @@
 package rtc.paritta.phiangthong.gameproverbs;
 
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
 /**
  * Created by masterUNG on 11/22/2016 AD.
  */
 
-public class SynQuestion    {
-}
+public class SynQuestion  extends AsyncTask<Void, Void, String>{
+
+    //Explicit
+    private static final String urlJSON = "http://swiftcodingthai.com/ary/get_question.php";
+    private Context context;
+
+    public SynQuestion(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+
+        try {
+
+        } catch (Exception e) {
+            Log.d("gameV1", "e doIn ==> " + e.toString());
+        }
+
+
+        return null;
+    }
+}   // Main Class
