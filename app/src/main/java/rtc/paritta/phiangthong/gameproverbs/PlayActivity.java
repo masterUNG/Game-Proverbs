fe3ff4f1b37419e8764b81b3172b1bc1ded37684
masterUNG <phrombutr@gmail.com>
2016-11-22 13:09:01 +0700

    design1 play

--- app/src/main/java/rtc/paritta/phiangthong/gameproverbs/PlayActivity.java
+++ app/src/main/java/rtc/paritta/phiangthong/gameproverbs/PlayActivity.java
@@ -9,5 +9,6 @@ public class PlayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
-    }
-}
+    }   // Main Method
+
+}   // Main Class
