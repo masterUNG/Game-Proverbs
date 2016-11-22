22b6a8e4ef36d563e314cc53dc2d7a8fab9224ca
masterUNG <phrombutr@gmail.com>
2016-11-22 14:17:47 +0700

    Count Time OK

--- app/src/main/java/rtc/paritta/phiangthong/gameproverbs/PlayActivity.java
+++ app/src/main/java/rtc/paritta/phiangthong/gameproverbs/PlayActivity.java
@@ -1,14 +1,80 @@
 package rtc.paritta.phiangthong.gameproverbs;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PlayActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView scoreTextView, timeTextView, timesTextView;
+    private ImageView imageView;
+    private Button choice0Button, choice1Button,choice2Button, choice3Button;
+    private int timeAnInt = 0, timesAnInt = 1, scoreAnInt = 0;
+    private boolean timeABoolean = false;   // false ==> ยังไม่หมดเวลา 120 วินาที
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        //Bind Widget
+        scoreTextView = (TextView) findViewById(R.id.textView);
+        timeTextView = (TextView) findViewById(R.id.textView2);
+        timesTextView = (TextView) findViewById(R.id.textView3);
+        imageView = (ImageView) findViewById(R.id.imageView3);
+        choice0Button = (Button) findViewById(R.id.button);
+        choice1Button = (Button) findViewById(R.id.button2);
+        choice2Button = (Button) findViewById(R.id.button3);
+        choice3Button = (Button) findViewById(R.id.button4);
+
+        //My Loop
+        myLoop();
+
+
     }   // Main Method
+
+    private void myLoop() {
+        //*****************
+        // To Do
+        //*****************
+
+
+        //Show Time
+        timeTextView.setText("เวลา " + Integer.toString(120 - timeAnInt) + " วินาที");
+        timeAnInt += 1;
+        if (timeAnInt == 120) {
+            timeABoolean = true;
+        }   // if
+
+
+
+
+        //Post Delay
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                if (!timeABoolean) {
+                    myLoop();
+                } else {
+                    //หมดเวลา 120 sec
+                    Intent intent = new Intent(PlayActivity.this, ShowScoreActivity.class);
+                    intent.putExtra("Score", scoreAnInt);
+                    startActivity(intent);
+                    finish();
+                }   // if
+
+            }   //run
+        }, 1000);
+
+    }   // myLoop
 
 }   // Main Class
---
+++ app/src/main/java/rtc/paritta/phiangthong/gameproverbs/SynQuestion.java
@@ -0,0 +1,8 @@
+package rtc.paritta.phiangthong.gameproverbs;
+
+/**
+ * Created by masterUNG on 11/22/2016 AD.
+ */
+
+public class SynQuestion {
+}
