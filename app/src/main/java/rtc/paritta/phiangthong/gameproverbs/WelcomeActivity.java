13628b8972b96d98145ec545a7d4e5185c7993ba
masterUNG <phrombutr@gmail.com>
2016-11-22 11:56:04 +0700

    Intent to Play finish

--- app/src/main/java/rtc/paritta/phiangthong/gameproverbs/WelcomeActivity.java
+++ app/src/main/java/rtc/paritta/phiangthong/gameproverbs/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package rtc.paritta.phiangthong.gameproverbs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-    }
-}
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView2);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(WelcomeActivity.this, PlayActivity.class));
+                finish();
+            }
+        });
+
+    }   // Main Method
+
+}   // Main Class
